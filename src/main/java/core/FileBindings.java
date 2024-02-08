f4fe7cbbcc2147e500eb5000344d9065d86789d5
Student <xcecha07@vutbr.cz>
2024-02-08 13:13:40 +0100

    added icon

---
+++ src/main/java/core/FileBindings.java
@@ -0,0 +1,14 @@
+package core;
+
+import javafx.scene.image.Image;
+
+import java.io.File;
+import java.net.URL;
+
+public class FileBindings {
+
+    public static final URL GUIMain = FileBindings.class.getClassLoader().getResource("graphics/MainWindow.fxml");
+
+    public static Image favicon = new Image(FileBindings.class.getClassLoader().getResourceAsStream("favicon.png"));
+
+}
